615691033f35b4a49d0b8afe8d23e69625edd704
Kallew Felip <85791728+yEuzy@users.noreply.github.com>
2025-09-21 13:10:54 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -64,9 +64,9 @@ btnPastel.addEventListener("click", function abrirorcao(){
 })
 
 let data = new Date();
-if(data.getDay() == 6){
+if(data.getDay() == "6"){
     divRefeicao.innerHTML += "<div id='marmitasabado' class='produtos'><table><tr><td><h2>Especial de Sabado</h2></td></tr><tr><td>Carne de Panela</td><td>30,00</td><td></td></tr><tr><td class='h2diferente'>Frango Assado</td><td class='h2diferente'>30,00</td></tr><tr><td><h3>Feijões disponiveis</h3></td></tr><tr><td>Feijao de Caldo</td><td>Feijao Preto c/ Calabresa</td></tr></table></div>"
 }
-if(data.getDay() == 1){
+if(data.getDay() == "1"){
     divRefeicao.innerHTML += "<div id='marmitadomingo' class='produtos'><table><tr><td><h2>Especial de Domingo</h2></td></tr><tr><td>Espetinho Contra File </td><td>30,00</td></tr><tr><td class='h2diferente'>Espetinho de Almondega</td><td class='h2diferente'>30,00</td></tr><tr><td>Espetinho Frambacon</td><td>28,00</td></tr><tr><td class='h2diferente'>Espetinho Linguiça</td><td class='h2diferente'>28,00</td></tr><tr><td>Espetinho Tulipa</td><td>28,00</td></tr><tr><td class='h2diferente'>Espetinho Coração</td><td class='h2diferente'>28,00</td></tr><tr><td>Carne de Panela</td><td>28,00</td><td></td></tr><tr><td><h3>Feijões disponiveis</h3></td></tr><tr><td>Feijao de Caldo</td><td>Feijao Tropeiro</td></tr></table></div>"
 }
